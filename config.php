2c04789e303750a6e83a4c82ee7861f549a717d9
ke7ijo <heiner@monstro.us>
2020-03-20 11:48:46 -0700

    typo

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
-$mysql_host = ""; // MySQL host address
-$mysql_user = ""; // MySQL username
+$mysql_host = "localhost"; // MySQL host address
+$mysql_user = "root"; // MySQL username
 $mysql_pass = ""; // MySQL password
 $mysql_db   = ""; // MySQL database name
 
-$imap_host  = "imap.gmail.com.993"; // IMAP host address
+$imap_host  = "imap.gmail.com:993"; // IMAP host address
 $imap_flags = "/imap/ssl/novalidate-cert"; // IMAP Flags
 $imap_user  = "username@gmail.com"; // IMAP username
 $imap_pass  = ""; // IMAP password
