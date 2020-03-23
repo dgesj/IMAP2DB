c9d7ed86f987271acdb445b888cc7047c242cb6d
ke7ijo <heiner@monstro.us>
2020-03-22 19:49:16 -0700

    change output directory

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 
 $mysql_host = "localhost"; // MySQL host address
 $mysql_user = "root"; // MySQL username
-$mysql_pass = ""; // MySQL password
-$mysql_db   = ""; // MySQL database name
+$mysql_pass = "vorac1ous"; // MySQL password
+$mysql_db   = "omnivore"; // MySQL database name
 
-$imap_host  = "imap.gmail.com:993"; // IMAP host address
+$imap_host  = "imap.example.com:993"; // IMAP host address
 $imap_flags = "/imap/ssl/novalidate-cert"; // IMAP Flags
-$imap_user  = "username@gmail.com"; // IMAP username
+$imap_user  = "username@example.com"; // IMAP username
 $imap_pass  = ""; // IMAP password
 
-$file_store = "files"; // Folder where file attachments are saved
+$file_store = "../output"; // Folder where file attachments are saved
 $grab_type  = "fetch"; // Type of mail grab - "pipe" or "fetch"
