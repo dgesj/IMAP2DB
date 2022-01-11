7d6b0e5949be4e7896c7775b8b23c55c1214c10e
S3bastian <6882236+S3bastian@users.noreply.github.com>
2022-01-11 19:09:59 +0100

    Update application.php

--- application.php
+++ application.php
@@ -1,20 +1,17 @@
 #!/usr/bin/php -q
 <?php
-
-//clone this script from rubo77/php-mysql-fix as required depending on your PHP version (aliases mysql/mysqli calls).
-include_once ("php-mysql-fix/fix_mysql.inc.php");
-
 require_once("config.php");
 require_once("class.php");
 require_once("mimeDecode.php");
 
+# Execution time
 set_time_limit(600);
 ini_set('max_execution_time',600);
 
-$mysql = mysql_connect($mysql_host, $mysql_user, $mysql_pass);
-$db_selected = mysql_select_db($mysql_db, $mysql);
-
-mysql_set_charset('utf8',$mysql);
+# MySQL connect
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+$mysqli->set_charset("utf8mb4");
 
 /* Pipe */
 if ($grab_type == "pipe") {
@@ -51,7 +48,7 @@ if ($grab_type == "fetch") {
         			foreach($emails AS $n) {
     	      				$source = imap_fetchbody($mailbox, $n, "");
           				$uniqid = generateId(20).date("U");
-    					$emailMessage = new EmailObject($mysql,$uniqid,$source,$file_store,$mailbox_name);
+    					$emailMessage = new EmailObject($mysqli,$uniqid,$source,$file_store,$mailbox_name);
           				$emailMessage->readEmail();
 	 				//The project forked by this one deletes fetched emails.
 	  				//This behavior must be explicitly called for (e.g. in config.php) in this project
@@ -87,4 +84,4 @@ function generateId($n) {
   return $id;
 }
 
-mysql_close($mysql);
+mysqli_close($mysqli);
